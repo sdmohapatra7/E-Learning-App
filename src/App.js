618a49e348fd630e074ec34329743fd5fb2819f1
Shaktidhar <sdmohapatra7@gmail.com>
2023-06-20 00:23:53 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import Hero from "./pages/app/hero/Hero";
+import Nav from "./components/nav/Nav";
+import Courses from "./pages/app/courses/Courses";
+import Chapter from "./pages/app/chapter/Chapter"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Details from "./pages/app/details/Details";
+import Learn from "./pages/app/learn/Learn";
+
+function App() {
+  const browserRouter = createBrowserRouter([
+    {
+      path: '/',
+      element: <Nav />,
+      children: [
+        { path: '', element: <Hero /> },
+        {
+          path: '/courses',
+          children: [
+            { index: true, element: <Courses /> },
+            { path: ':courseId', element: <Details /> }
+          ]
+        },
+        {path:'/learn/:courseId',
+        element:<Learn />,
+        children:[
+          {path:'chapter/:chapterId',element:<Chapter />}
+        ]
+      }
+      ]
+    }
+  ])
+  return (
+    <div className="App">
+      <RouterProvider router={browserRouter} />
+    </div>
+  );
+}
+
+export default App;
